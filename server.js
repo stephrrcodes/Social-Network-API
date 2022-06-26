d420d236499391c56bffa35d28909d5d39cbfb45
stephrrcodes <98715697+stephrrcodes@users.noreply.github.com>
2022-06-25 17:34:30 -0700

    server.js started

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
